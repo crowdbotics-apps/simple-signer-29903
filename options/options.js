a7cc59da354a4f595dd1f43b5845ea8bb1b4f841
Crowdbotics <team@crowdbotics.com>
2021-08-18 06:45:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "simple_signer_29903Identifier",
+  url: "https://simple_signer_29903Identifier.botics.co",
+  api: "https://simple_signer_29903Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
